102216f9dab4ff638fcd97c8114e16e89fb8e6bd
tarun003k <tarunkumarmalladi2004@gmail.com>
2025-03-13 13:41:31 +0530

    Removed all files from the repository

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for smooth scrolling
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
